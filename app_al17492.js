36b0a8f627fce67bd99f3cd803b43965ee77e101
ghimel cruz garcia <al17492@cbtis051.edu.mx>
2019-12-03 15:06:38 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17492.js
+++ app_al17492.js
@@ -1,7 +1,14 @@
 var http = require('http');
 http.createServer(funcion (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo Git - Cruz Garcia Ghimel\n');
-}).listen(8081);
-console.log('Server running on port 8081');
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+   
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo GIT - Cruz Garcia Ghimel\n'};
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
+
+    }).listen(8081);
+    console.log('Server running on port 8081');    
