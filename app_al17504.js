7765dcd3249444a7bce3de71c2ef85efdda8bfad
Loya Soria Jason Bary <al17504@cbtis051.edu.mx>
2019-12-03 14:37:37 -0700

    Commit inicial del server

---
+++ app_al17504.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+
+    res.end ('Hola mundo Node JS Repo GIT - Loya Soria Jason Baey \n');
+}).listen(8081);
+console.log('Server running on port 8081');
